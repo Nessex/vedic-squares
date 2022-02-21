00cf2e5adca1af94e5d1bba7fcda6ae990e12282
Nathan Essex <nathan@essex.id.au>
2022-02-22 00:59:09 +0900

    mvp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const tableStyle = {
+  border: '1px solid #CCC',
+  borderCollapse: 'collapse',
+};
+
+const tableCellStyle = {
+  border: '1px solid #CCC',
+  width: '2em',
+  height: '2em',
+}
+
+const cellStyle = function(colors, base, val) {
+  let idx = parseInt(val, base);
+
+  let out = Object.assign({}, tableCellStyle);
+
+  out.backgroundColor = colors[idx];
+
+  return out;
+}
+
+const randomColor = () => "#" + ((1<<24)*Math.random() | 0).toString(16);
+
 function App() {
+  const [base, setBase] = useState(7);
+  const [data, setData] = useState([]);
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    let mulTable = [];
+    for (let y = 1; y < base; y++) {
+      for (let x = 1; x < base; x++) {
+        mulTable.push((x * y).toString(base));
+      }
+    }
+
+    const digitalRoots = mulTable.map(val => {
+      while (val.length > 1) {
+        val = val.split("")
+            .map(d => parseInt(d, base))
+            // Sum all values
+            .reduce((p, c) => p + c, 0).toString(base);
+      }
+
+      return val;
+    }).reverse();
+
+    let rows = [];
+
+    for (let x = 0; x < base - 1; x++) {
+      let row = [];
+      for (let y = 0; y < base - 1; y++) {
+        row.push(digitalRoots.pop());
+      }
+
+      rows.push(row);
+    }
+
+    setData(rows);
+
+    let colors = [];
+    for (let i = 0; i < base; i++) {
+      colors.push(randomColor());
+    }
+
+    setColors(colors);
+
+  }, [base]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>
+          Numeric Base:&nbsp;
+          <input id="base-input" type="number" min={2} max={36} defaultValue={base} onChange={(e) => setBase(parseInt(e.target.value))} />
+        </label>
+
+        <table style={tableStyle}>
+          { data.map(row =>
+            <tr>
+              { row.map(v =>
+                  <td style={cellStyle(colors, base, v)}>{v}</td>
+              )}
+            </tr>
+          )}
+        </table>
       </header>
     </div>
   );
